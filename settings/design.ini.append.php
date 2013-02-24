03324d600dd899f1e4403b57ad86c93fe81d6dc2
Piotr Szczygiel <piotr.szczygiel@makingwaves.pl>
2013-02-24 19:17:56 +0100

    fixed the mess with JS and CSS files

--- settings/design.ini.append.php
+++ settings/design.ini.append.php
@@ -3,8 +3,4 @@
 [ExtensionSettings]
 DesignExtensions[]=formmaker
 
-[JavaScriptSettings]
-JavaScriptList[]=http://code.jquery.com/jquery-1.9.1.min.js
-JavaScriptList[]=http://code.jquery.com/ui/1.9.2/jquery-ui.js
-
  */
