1f15d66837d573863214a1090cb121b70b56ada7
Piotr Szczygiel <piotr.szczygiel@makingwaves.pl>
2013-07-22 14:28:51 +0200

    Removed deprecated .live() jQuery method - it's replaced by .on()

--- design/admin2/javascript/edit.js
+++ design/admin2/javascript/edit.js
@@ -1,7 +1,7 @@
 jQuery(document).ready( function() {
 
     // remove form attribute
-    jQuery('.formField .remove-field').live('click', function() {
+    jQuery( document.body ).on( 'click', '.formField .remove-field', function(){
         if (confirm($('#dialog-confirm').html())){
             var obj = $(this).parents('.formField');
             obj.hide(500, function(){
@@ -11,25 +11,25 @@ jQuery(document).ready( function() {
     });
 
     // setting the default checkbox value for mandatory checkbox
-    jQuery('.attribute-mandatory-holder input[type=checkbox]').live('click', function() {
+    jQuery( document.body ).on( 'click', '.attribute-mandatory-holder input[type=checkbox]', function(){
         var value = 0;
         if ($(this).is(':checked')) {
             value = 'on';
-        } 
+        }
         $(this).parent('.attribute-mandatory-holder').find('input[type=hidden]').val(value);
     });
     
     // setting the default checkbox value for email receiver checkbox
-    jQuery('.email-receiver-inputs input[type=checkbox]').live('click', function() {
+    jQuery( document.body ).on( 'click', '.email-receiver-inputs input[type=checkbox]', function(){
         var value = 0;
         if ($(this).is(':checked')) {
             value = 1;
-        } 
+        }
         $(this).parent('span').find('input[type=hidden]').val(value);
         $(this).parents('.form-field-attributes-container')
                .find('.dynamic-validator-value-' + $('#email-validator-id').val())
                .val(value);
-    });    
+    });
 
     // Enabling the drag'n'drop
     jQuery( ".sortable-attributes" ).sortable({
@@ -57,13 +57,13 @@ jQuery(document).ready( function() {
     });
     
     // Adding new option line to attribute
-    jQuery('.formField .add-option').live('click', function(){
+    jQuery( document.body ).on( 'click', '.formField .add-option', function(){
         jQuery('.form-ajax-loader').show();
         var post_data = {'attribute_id' :  $(this).parents('.formField').find('.attribute-unique-id').val()};
         var object = $(this);
-        
+
         $.ez( 'formmaker::addAttributeOption', post_data, function( data ) {
-            
+
             if (data.error_text) {
                 alert(data.error_text);
             }
@@ -71,19 +71,19 @@ jQuery(document).ready( function() {
                 object.parent('.form-attribute-options').find('ul').append(data.content);
             }
             jQuery('.form-ajax-loader').hide();
-        });         
+        });
     });
     
     // Removing option line from attribute
-    jQuery('.form-attribute-options .option-remove').live('click', function(){
+    jQuery( document.body ).on( 'click', '.form-attribute-options .option-remove', function(){
         if ($(this).parents('.form-attribute-options ul').find('li').length > 1) {
             if (confirm($('#dialog-confirm').html())){
-            
+
                 // remove default value from hidden field if current option is checked
                 if ($(this).parent().find('.default-radio input[type=checkbox]').is(':checked')){
                     $(this).parents('.formField').find('input[name="' + $(this).parent().find('.default-radio input[type=checkbox]').attr('connected') + '"]').val('');
                 }
-                
+
                 var obj = $(this).parent();
                 obj.hide(500, function(){
                     obj.remove();
@@ -93,54 +93,53 @@ jQuery(document).ready( function() {
     });
     
     // Moving option line up
-    jQuery('.form-attribute-options .option-move-up').live('click', function(){
+    jQuery( document.body ).on( 'click', '.form-attribute-options .option-move-up', function(){
         var current_index = $($(this).parents('ul').find('.option-move-up')).index(this);
         if (current_index > 0) {
             var current_element = $(this).parents('ul').find('li').get(current_index);
             $(current_element).prev().before(current_element);
         }
     });
-    
+
     // Moving option line down
-    jQuery('.form-attribute-options .option-move-down').live('click', function(){
+    jQuery( document.body ).on( 'click', '.form-attribute-options .option-move-down', function(){
         var current_index = $($(this).parents('ul').find('.option-move-down')).index(this);
         if (current_index+1 < $(this).parents('ul').find('.option-move-down').length) {
             var current_element = $(this).parents('ul').find('li').get(current_index);
             $(current_element).next().after(current_element);
         }
-    });  
-    
+    });
+
     // setting default value for radio options
-    jQuery('.default-radio input[type=checkbox]').live('click', function(){
-        
+    jQuery( document.body ).on( 'click', '.default-radio input[type=checkbox]', function(){
         var hidden_value = '';
-        
+
         if ($(this).is(':checked')) {
             var option_id = $(this).attr('option_id');
             hidden_value = option_id;
-            
+
             // setting the value to hidden field
             $(this).parents('.formField').find('input[name="' + $(this).attr('connected') + '"]').val(option_id);
-            
+
             // unchecking other checkboxes from this group
             $(this).parents('.form-attribute-options').find('input[type=checkbox][option_id!=' + option_id + ']:checked').attr('checked', false);
         } else {
             // for security reasons, unchecking all checkboxes
             $(this).parents('.form-attribute-options').find('input[type=checkbox]').attr('checked', false);
         }
-        
+
         // setting hidden field value
-        $(this).parents('.formField').find('input[name="' + $(this).attr('connected') + '"]').val(hidden_value);        
+        $(this).parents('.formField').find('input[name="' + $(this).attr('connected') + '"]').val(hidden_value);
     });
     
     // "enable" checkbox
-    jQuery('.enable-attribute input[type=checkbox]').live('click', function(){
+    jQuery( document.body ).on( 'click', '.enable-attribute input[type=checkbox]', function(){
         var value = 0;
         var separator_id = $('#separator-id').val();
         if ($(this).is(':checked')) {
             value = 1;
         }
-        
+
         // disabling/enabling whole form page
         if ($(this).parents('.formField').find('input[type=hidden][name$="[type]"]').val() === separator_id) {
             var all_formfields = $('.sortable-attributes .formField');
@@ -158,19 +157,19 @@ jQuery(document).ready( function() {
                 checkbox.parent().find('input[type=hidden]').val(value);
             }
         }
-        
+
         $(this).parent().find('input[type=hidden]').val(value);
     });
 
     // Storing custom regex validator content
-    $('.custom-regex-inputs > input[type=text]').live('blur', function(){
+    jQuery( document.body ).on( 'blur', '.custom-regex-inputs > input[type=text]', function(){
         $(this).parents('.form-field-attributes-container')
                .find('.dynamic-validator-value-' + $('#custom-regex-validator-id').val())
                .val($(this).val());
     });
-    
+
     // displaying and hiding email receiver and custom regex fields (when validator select list changes)
-    jQuery('.attribute-validation').live('change', function(){
+    jQuery( document.body ).on( 'click', '.attribute-validation', function(){
 
         if ($.inArray($(this).val(), $('#form-dynamic-validators').val().split(',')) > -1) {
 
@@ -181,7 +180,7 @@ jQuery(document).ready( function() {
                 'value'         : $(this).parents('.form-field-attribute').find('.dynamic-validator-value-' + $(this).val().toString()).val()
             };
             var object = $(this);
-            
+
             $.ez( 'formmaker::addDynamicAttribute', post_data, function( data ) {
                 if (data.error_text) {
                     alert(data.error_text);
@@ -191,7 +190,7 @@ jQuery(document).ready( function() {
                     object.parents('.form-field-attributes-container').append(data.content);
                 }
                 jQuery('.form-ajax-loader').hide();
-            });  
+            });
         } else {
             $(this).parents('.form-field-attributes-container').find('.dynamic-validator-holder').remove();
         }
