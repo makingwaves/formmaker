f870737829924bf16c08c4ec508cd41864654294
Karol Bera <karol.bera@gmail.com>
2014-07-10 14:11:45 +0200

    left menu continued

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -2,12 +2,16 @@
 
 namespace MakingWaves\FormMakerBundle\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use eZ\Bundle\EzPublishCoreBundle\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
-    public function indexAction($name)
+    public function testAdminAction(Request $request, $someParam = null)
     {
-        return $this->render('FormMakerBundle:Default:index.html.twig', array('name' => $name));
-    }
-}
+        return $this->render(
+            'FormMakerBundle:Default:test.html.twig',
+            array('some_param' => $someParam)
+        );
+    } // testAdminAction
+} // class DefaultController
